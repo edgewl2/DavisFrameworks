321b26e1a6fd3d074c67388323d3754ac3a2a1b2
Luis Solórzano <luisnic20@gmail.com>
2016-08-22 08:51:46 -0600

    Create Class Views and add Twig in Core DavisFramework

--- app/WorkSpace/Views/layouts/app.temp.php
+++ app/WorkSpace/Views/layouts/app.temp.php
@@ -5,6 +5,7 @@
 	<title>DavisFramework</title>
 	<link href="app/Sites/theme/bootstrap-v4/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="app/Sites/theme/bootstrap-v4/assets/css/layouts.css" rel="stylesheet" type="text/css">
+	<link href="app/Sites/theme/bootstrap-v4/assets/css/css.css" rel="stylesheet" type="text/css">
 	<script src="app/Sites/theme/bootstrap-v4/assets/js/bootstrap.min.js"></script>
 </head>
 <body>
